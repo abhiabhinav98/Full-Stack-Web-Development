ff81366af79f17fa7e618573b34b2ee902e78d84
abhiabhinav98 <abhiabhinav98@gmail.com>
2020-05-03 15:55:51 +0530

    presentational components

--- Front End Web Development with React/confusion/src/components/MenuComponent.js
+++ Front End Web Development with React/confusion/src/components/MenuComponent.js
@@ -1,32 +1,26 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import Dishdetail from './DishdetailComponent'
+
 
 class Menu extends Component{
 
     constructor(props) {
         super(props);
 
-        this.state = {
-            selectedDish: null
-        }
-        console.log('constructor is invoked');
     }
 
     componentDidMount(){
         console.log('menu comp compdidmount is invoked');
     }
 
-    onDishSelect(dish){
-        this.setState({ selectedDish: dish});
-    }
+    
 
     render(){
 
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={() => this.props.onClick(dish.id) }>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -42,10 +36,10 @@ class Menu extends Component{
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
+                {/* <div className="row">
                     <Dishdetail dish={this.state.selectedDish} />
-                    {/* {this.renderDish(this.state.selectedDish)} */}
-                </div>
+                     {this.renderDish(this.state.selectedDish)} 
+                </div> */}
             </div>
         );
     }

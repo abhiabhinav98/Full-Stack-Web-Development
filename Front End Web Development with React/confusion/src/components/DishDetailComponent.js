7fdcda6827ccb2eaf2f1db4a2e326b8bc8b4c5df
abhiabhinav98 <abhiabhinav98@gmail.com>
2020-05-16 19:04:45 +0530

    promise

--- Front End Web Development with React/confusion/src/components/DishDetailComponent.js
+++ Front End Web Development with React/confusion/src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardBody,CardText, CardTitle, BreadcrumbItem, Breadcrumb
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
     const required = (val) => val && val.length;
@@ -107,7 +108,7 @@ import { Loading } from './LoadingComponent';
         if (dish != null) {
             return(
                 <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>

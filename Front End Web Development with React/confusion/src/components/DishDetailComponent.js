7d2c631373bf1abe54c3caa081e04d70addc5494
abhiabhinav98 <abhiabhinav98@gmail.com>
2020-05-16 20:22:45 +0530

    stagger animation 2

--- Front End Web Development with React/confusion/src/components/DishDetailComponent.js
+++ Front End Web Development with React/confusion/src/components/DishDetailComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
     const required = (val) => val && val.length;
@@ -107,13 +108,17 @@ import { baseUrl } from '../shared/baseUrl';
     function RenderDish({dish}) {
         if (dish != null) {
             return(
-                <Card>
-                    <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <FadeTransform in transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                    <Card>
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </FadeTransform>
             )
         }
         else{
@@ -125,31 +130,37 @@ import { baseUrl } from '../shared/baseUrl';
 
     function RenderComments({comments, postComment, dishId}){
         if (comments != null) {
-
-            let list = comments.map((comments)=>{
-
-                let date = comments.date
-
-                return(
-                    <li key={comments.id} >
-                        <div>
-                            <p>{comments.comment}</p>
-                            <p>--{comments.author}, {formatDate({date})}</p>
-                        </div>
-                    </li>
-
-                )
-            })
-
             return(
-                <div>
+                <div className='col-12 m-1'>
                     <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        {list}
+                    <ul className='list-unstyled'>
+                    <Stagger in>
+                        {comments.map((comment) => {
+                            return (
+                                <Fade in>
+                                <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                                </Fade>
+                            );
+                        })}
+                        <CommentForm dishId={dishId} postComment={postComment} />  
+                        </Stagger>
                     </ul>
-                    <CommentForm dishId={dishId} postComment={postComment} />    
                 </div>
             )
+                
+
+            //return(
+            //    <div>
+            //        <h4>Comments</h4>
+            //        <ul className="list-unstyled">
+            //            {list}
+            //        </ul>
+                      
+            //    </div>
+            //)
         }
         else{
             return(
